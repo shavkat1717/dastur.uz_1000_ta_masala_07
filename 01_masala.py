ef963251cadf221ab3da4efa142bdd02a7096ec7
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-11-18 22:02:31 +0500

    While sikli operatoriga oid masalalar
    While sikli operatoriga oid masalalar

---
+++ 01_masala.py
@@ -0,0 +1,8 @@
+a=int(input("A kesmani kiriting A => "))
+b=int(input("B kesmani kiriting B => "))
+if a>b:
+    while (a>b):
+        a=a-b
+    print(a)
+else:
+    print("A > B shartiga e'tibor bering!")
